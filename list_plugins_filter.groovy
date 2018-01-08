a0375dece64d2c325aa07f749455cfd33aaf9551
Sudheer Veeravalli <veersudhir83@gmail.com>
2018-01-08 15:33:10 +0530

    Create list_plugins_filter.groovy

---
+++ list_plugins_filter.groovy
@@ -0,0 +1,16 @@
+#!/usr/bin/env groovy
+
+/**
+ * @ Maintainer sudheer veeravalli<veersudhir83@gmail.com>
+ * Lists all plugins and filters out any blue ocean plugins if not needed
+ */
+ 
+def plugins = Jenkins.instance.pluginManager.plugins
+allPlugins = plugins.collect{it}
+allPlugins = allPlugins.sort()
+def finalList = []
+for(String item in allPlugins) {
+  //if (item.indexOf("blue", 0) < 0) { // enable this condition to filter out any blue ocean plugins
+    finalList.add(item.substring(7))
+  //}
+}

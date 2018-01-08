795248e187b45d6c23fd5fdea91d08a93b9b2846
Sudheer Veeravalli <veersudhir83@gmail.com>
2018-01-08 15:43:51 +0530

    Update get-build-information.groovy

--- get-build-information.groovy
+++ get-build-information.groovy
@@ -10,7 +10,7 @@
  .find{job->job.name.contains('dsl')}
  .find{ [whatever]}
 */
-Jenkins.instance.getAllItems(Job).find{job->job.name.equals('JOB_NAME')}.each{ // adds filter to search for specific job
+Jenkins.instance.getAllItems(Job).find{job->job.name.equals('ePA-Test-Execution')}.each{ // adds filter to search for specific job
 
   def jobBuilds=it.getBuilds().limit(10) // limit number of builds to fetch
 
@@ -19,17 +19,16 @@ Jenkins.instance.getAllItems(Job).find{job->job.name.equals('JOB_NAME')}.each{ /
       def runningSince = groovy.time.TimeCategory.minus( new Date(), build.getTime() )
       def currentStatus = build.buildStatusSummary.message
       def cause = build.getCauses()[0] //we keep the first cause
-      //This is a simple case where we want to get information on the cause if the build was 
-      //triggered by an user
-      def user = cause instanceof Cause.UserIdCause? cause.getUserId():""
+      //This is a simple case where we want to get information on the cause if the build was triggered by an user
+      def user = cause instanceof Cause.UserIdCause? cause.getUserId():"Trigger"
       //This is an easy way to show the information on screen but can be changed at convenience
-      println "Build: ${build} | Since: ${runningSince} | Status: ${currentStatus} | Cause: ${cause} | User: ${user}" 
+      println "Build: ${build} | Last Run: ${runningSince} | Status: ${currentStatus} | Cause: ${cause} | User: ${user}" 
      
       // You can get all the information available for build parameters.
-      def parameters = build.getAction(ParametersAction)?.parameters
+      /*def parameters = build.getAction(ParametersAction)?.parameters
       parameters.each {
         println "Type: ${it.class} Name: ${it.name}, Value: ${it.dump()}" 
       
-      }
+      }*/
    }
 }

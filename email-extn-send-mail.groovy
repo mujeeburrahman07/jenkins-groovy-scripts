65b5677eb3bd38837dcb5100eac920cbe905cca0
Sudheer Veeravalli <veersudhir83@gmail.com>
2018-01-09 16:12:26 +0530

    Update email-extn-send-mail.groovy

--- email-extn-send-mail.groovy
+++ email-extn-send-mail.groovy
@@ -1,13 +1,19 @@
 node {
-    notify('Started')
-   // mail bcc: '', body: 'Hi', cc: '', from: 'noreply@jenkins-quest-global.com', replyTo: '', subject: 'Hi', to: 'sudheer.veeravalli@quest-global.com'
+    stage 'Question'
+    echo "Pipeline Started"
+    input 'Run the build?'
+    notify('Want to run the build?')
+    
+    stage 'Parallel'
+    parallel p1: { echo "p1" }, p2:{ echo "p2" }
 }
+
 def notify(status) {
 	emailext (
 	  to: "${mailRecipients}",
 	  replyTo: "noreply@jenkins.com",
 	  subject: "${status}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'",
 	  body: """<p>${status}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
-	      <p>Check console output at <a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</p>"""
+	      <p>Check console output at <a href='${env.BUILD_URL}console'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</p>"""
 	)
 }
